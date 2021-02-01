85cc01142aebed6f6feeb802c28c08d25bf99229
vd89 <dixitvora@gmail.com>
2021-02-01 21:31:03 +0530

    SSR_Chore: Added the nodemon and script

--- server/src/client/Component/Home.js
+++ server/src/client/Component/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
   return (
     <div>
-      <h1>I'm the home component</h1>
+      <h1>I'm the This is Working home component</h1>
     </div>
   );
 };
