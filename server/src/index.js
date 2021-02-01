a88a0337877945657f477a365e6d19477c141de7
vd89 <dixitvora@gmail.com>
2021-02-01 21:17:43 +0530

    SSR_Chore: set up the server to build

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const React = require('react');
+const renderToString = require('react-dom/server').renderToString;
+const Home = require('./client/Component/Home').default;
+
 const app = express();
 
-app.get('/', (req, res) => {});
+app.get('/', (req, res) => {
+  const content = renderToString(<Home />);
+  res.send(content);
+});
 
 app.listen(3000, () => {
   console.log('Server is listening on port 3000');
